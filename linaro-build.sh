fdc15c705f71c351eb0af6cac58900efbbec0a48
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2011-08-16 16:18:08 +0159

    Use binutils 2.21.53.0.2 rather than 2.21.53.0.1, it comes with a couple of arm fixes

--- linaro-build.sh
+++ linaro-build.sh
@@ -265,7 +265,7 @@ ${ARG_TOOLCHAIN_SRC_DIR}/build/configure \
   \
   ${BUILD_WITH_GCC} \
   ${BUILD_WITH_GDB} \
-  --with-binutils-version=2.21.53.0.1 \
+  --with-binutils-version=2.21.53.0.2 \
   \
   --with-gmp-version=5.0.2 \
   --with-mpfr-version=3.0.1 \

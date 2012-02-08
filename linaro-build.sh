f67dd2b4f7065f1406d482d6329853f0bfc4a6e5
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2012-02-08 03:07:50 +0100

    toolchain/build: Default to using binutils 2.22.52.0.1
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- linaro-build.sh
+++ linaro-build.sh
@@ -265,7 +265,7 @@ ${ARG_TOOLCHAIN_SRC_DIR}/build/configure \
   \
   ${BUILD_WITH_GCC} \
   ${BUILD_WITH_GDB} \
-  --with-binutils-version=${BINUTILS_VERSION-2.22.51.0.1} \
+  --with-binutils-version=${BINUTILS_VERSION-2.22.52.0.1} \
   \
   --with-gmp-version=${GMP_VERSION-5.0.3} \
   --with-mpfr-version=${MPFR_VERSION-3.1.0} \
